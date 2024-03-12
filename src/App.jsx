dd6d5ac4fd8b8fdc91d9cc228f977274cdf18d38
zain1501 <zainmomin049@gmail.com>
2024-03-12 17:47:11 +0530

    api-react-vite

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Saved from "./components/Saved";
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [search, setSearch] = useState("nature");
+  const [loader, setLoader] = useState(true);
+  const [saved, setSaved] = useState([]);
+
+  const API_KEY = "XZrCeB2MUkeI9OhJsN12KJPwoRB1i6VAEIAAchuJmTZEDCSQ3OY603yY";
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const res = await axios.get(
+        `https://api.pexels.com/v1/search?query=${search}&per_page=100`,
+        {
+          headers: {
+            Authorization: API_KEY,
+          },
+        }
+      );
+      //  console.log("response from API = ",res.data.photos)
+      setLoader(false);
+      setImages(res.data.photos);
+      console.log(images);
+    };
+
+    const data = JSON.parse(localStorage.getItem("Images"));
+    if (data) {
+      setSaved(data);
+    }
+
+    fetchImage();
+  }, [search]);
+
+  useEffect(() => {
+    if (saved.length != 0) {
+      const json = JSON.stringify(saved);
+      localStorage.setItem("Images", json);
+    }
+  }, [saved]);
+
+  console.log("Image is saved ", saved);
+
+  return (
+    <>
+      <Router>
+        <Navbar setSearch={setSearch} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                images={images}
+                loader={loader}
+                saved={saved}
+                setSaved={setSaved}
+              />
+            }
+          />
+          <Route
+            path="/saved"
+            element={<Saved saved={saved} loader={loader} />}
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Loader from "./Loader";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Home = ({ images, loader, setSaved, saved }) => {
+  const saveImage = (img) => {
+    let flag = true;
+
+    if (saved !== null && saved.length > 0) {
+      for (let i = 0; i < saved.length; i++) {
+        if (saved[i].id === img.id) {
+          flag = false;
+
+          toast.info("Image alredy saved", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Bounce,
+          });
+          break;
+        }
+      }
+    }
+    if (flag) {
+      setSaved([...saved, img]);
+
+      toast.success("Image Saved", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
+    }
+  };
+  return (
+    <>
+      <ToastContainer />
+      <div className="container-fluid text-center" id="top">
+        {loader ? (
+          <Loader />
+        ) : (
+          <>
+            <div className="flex">
+              {images.map((image) => (
+                <div
+                  key={image.id}
+                  className="items"
+                  onClick={() => saveImage(image)}
+                >
+                  <img src={image.src.medium} alt={image.photographer} />
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+
+        {images.length != 0 && (
+          <a href="#top" className="btn btn-warning my-5">
+            Back To Top
+          </a>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Home;
